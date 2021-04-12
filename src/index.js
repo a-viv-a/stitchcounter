979b1f2dec61172c732851ad240163860a08c246
isaac <isaac@rubenfamily.com>
2021-04-11 22:44:31 -0700

    basic javascript, remove unused file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import { yep } from "./stitch.js"
+class Counter {
+    constructor() {
+        this.element = document.getElementById("countNumber")
+    }
+    set number(value) {
+        value = Math.max(0, value)
+        removeButton.disabled = value <= 0
+        //console.log(`counter set to ${value}`)
+        this.element.textContent = value
+    }
+    get number() { return parseInt(this.element.textContent) }
+}
 
-console.log(yep)
+let countNumber = new Counter()
+let buttonArray = []
+    ;["addButton", "removeButton", "mod1", "mod3", "mod5", "mod10"].forEach((id) => { buttonArray.push(document.getElementById(id)) })
+let [addButton, removeButton, mod1, mod3, mod5, mod10] = buttonArray
+addButton.addEventListener("click", () => countNumber.number++)
+removeButton.addEventListener("click", () => countNumber.number--)
