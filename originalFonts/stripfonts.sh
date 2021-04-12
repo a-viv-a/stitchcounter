4dff693e88caa500489a456bfc320993d4e64bd4
isaac <isaac@rubenfamily.com>
2021-04-12 11:56:22 -0700

    remove unneeded chars from fonts

---
+++ originalFonts/stripfonts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo 'removing unneeded chars from fonts'
+pyftsubset lexend-v2-latin-regular.woff --output-file=../src/fonts/lexend-v2-stripped-regular.woff --text="0123456789 rowstichune" --with-zopfli
+pyftsubset lexend-v2-latin-regular.woff2 --output-file=../src/fonts/lexend-v2-stripped-regular.woff2 --text="0123456789 rowstichune"
