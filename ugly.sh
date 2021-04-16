78e157681bc7e76067c6fb33b5092045eee145ef
isaac <isaac@rubenfamily.com>
2021-04-15 17:39:49 -0700

    add bash script to minify js in place

---
+++ ugly.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+terser -c drop_console -m --mangle-props reserved=[increment,stitches,cRowStitches] --module -o src/index.js -- src/index.js
+terser -c drop_console -m --module -o src/sw.js -- src/sw.js
