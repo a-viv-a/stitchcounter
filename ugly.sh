11606fdf04d769f6e8837841da11a15b7e1d2241
isaac <isaac@rubenfamily.com>
2021-04-15 18:51:52 -0700

    use npx to fix ugly.sh

--- ugly.sh
+++ ugly.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-terser -c drop_console -m --mangle-props reserved=[increment,stitches,cRowStitches] --module -o src/index.js -- src/index.js
-terser -c drop_console -m --module -o src/sw.js -- src/sw.js
-cleancss -o main.css main.css
+npx terser -c drop_console -m --mangle-props reserved=[increment,stitches,cRowStitches] --module -o src/index.js -- src/index.js
+npx terser -c drop_console -m --module -o src/sw.js -- src/sw.js
+npx clean-css-cli -o src/main.css src/main.css
