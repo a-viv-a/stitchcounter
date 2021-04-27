0d8c79a2a0e7d66d5349bbd45ded3f8ca94a3b68
isaac <isaac@rubenfamily.com>
2021-04-27 16:12:50 -0700

    add punctuation

--- originalFonts/stripfonts.sh
+++ originalFonts/stripfonts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'removing unneeded chars from fonts'
-pyftsubset lexend-v2-latin-regular.woff --output-file=../src/fonts/lexend-v2-stripped-regular.woff --text="0123456789 abcdefghijklmnopqrstuvwxyz+-" --with-zopfli
-pyftsubset lexend-v2-latin-regular.woff2 --output-file=../src/fonts/lexend-v2-stripped-regular.woff2 --text="0123456789 abcdefghijklmnopqrstuvwxyz+-" --flavor=woff2
+pyftsubset lexend-v2-latin-regular.woff --output-file=../src/fonts/lexend-v2-stripped-regular.woff --text="0123456789 abcdefghijklmnopqrstuvwxyz+-!." --with-zopfli
+pyftsubset lexend-v2-latin-regular.woff2 --output-file=../src/fonts/lexend-v2-stripped-regular.woff2 --text="0123456789 abcdefghijklmnopqrstuvwxyz+-!." --flavor=woff2
