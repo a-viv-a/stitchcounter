68898aa5d72dfec8de39ae5176f1c73dfdff1a1d
isaac <isaac@rubenfamily.com>
2021-04-27 20:58:39 -0700

    add to help menu

--- originalFonts/stripfonts.sh
+++ originalFonts/stripfonts.sh
@@ -2,4 +2,4 @@
 
 echo 'removing unneeded chars from fonts'
 pyftsubset lexend-v2-latin-regular.woff --output-file=../src/fonts/lexend-v2-stripped-regular.woff --text="0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-!.,>" --with-zopfli
-pyftsubset lexend-v2-latin-regular.woff2 --output-file=../src/fonts/lexend-v2-stripped-regular.woff2 --text="0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-!.,>" --flavor=woff2
+pyftsubset lexend-v2-latin-regular.woff2 --output-file=../src/fonts/lexend-v2-stripped-regular.woff2 --text="0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-!.,'()>" --flavor=woff2
